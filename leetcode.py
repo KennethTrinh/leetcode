7fa5c112a5296ae29c7299f0767201fd304278c5
Kenneth Trinh <klt45@cornell.edu>
2022-12-26 12:13:38 -0500

    Weekly Contest 321

--- leetcode.py
+++ leetcode.py
@@ -144,7 +144,22 @@ class Trie:
         return 'end' in t
 
 
+def subarrays(arr):
+    """
+    A subarray is a contiguous part of array and maintains relative ordering of elements. 
+    For an array/string of size n, there are n*(n+1)/2 non-empty subarrays/substrings.
+    """
+    return [arr[i:j] for i in range(len(arr)) 
+            for j in range(i+1, len(arr)+1)]
+
+
+
+
 def subsets(arr):
+    """
+    A subset MAY NOT maintain relative ordering of elements and can or cannot be a contiguous part of an array. 
+    For a set of size n, we can have (2^n) sub-sets in total.  Same as subsequences but with the empty set included.
+    """
     result = [[]]
     for num in arr:
         result += [curr + [num] for curr in result]
@@ -177,3 +192,133 @@ class UF:
         rootX = self.find(x)
         rootY = self.find(y)
         self.uf[ rootX ] = rootY
+
+nums = [3,2,1,4,5]
+k = 2
+def bruteForce(nums, k):
+    getMedian = lambda arr: sorted(arr)[len(arr)//2] if len(arr) % 2 else sorted(arr)[len(arr)//2 - 1]
+    for arr in subarrays(nums):
+        if getMedian(arr) == k:
+            print(arr)
+
+# bruteForce(nums, k)
+"""
+[3], [3, 2], [3, 2, 1], [3, 2, 1, 4], [3, 2, 1, 4, 5], 
+[2], [2, 1], [2, 1, 4], [2, 1, 4, 5], 
+[1], [1, 4], [1, 4, 5], [4], [4, 5], [5]
+"""
+
+def countSubarrays(nums, k):
+    index = nums.index(k)
+    
+    res = l1 = s1 = 0
+    before = defaultdict(int)
+
+    for i in range(index-1,-1,-1):
+        if nums[i] > k: l1 += 1
+        else: s1 += 1
+        before[(l1-s1)] += 1
+        
+        if l1-s1==1 or l1-s1==0: res += 1
+
+    l2 = s2 = 0
+    for i in range(index+1,len(nums)):
+        if nums[i] > k: l2 += 1
+        else: s2 += 1
+        res += before[s2-l2] + before[s2-l2+1]
+        
+        if l2-s2==1 or l2-s2==0: res += 1
+    
+    print(l1, s1, l2, s2, before)
+    return res+1
+
+# print(countSubarrays(nums, k))
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    @staticmethod
+    def generateList(arr):
+        head = ListNode(arr[0])
+        ptr = head
+        for i in range(1, len(arr)):
+            ptr.next = ListNode(arr[i])
+            ptr = ptr.next
+        return head
+    def printList(self):
+        ptr = self
+        while ptr:
+            print(ptr.val, end=" ")
+            ptr = ptr.next
+        print()
+
+
+arr = [5,2,13,3,8] 
+head = ListNode.generateList(arr)
+# head.printList()
+
+
+def reverseList(head):
+    if not head:
+        return head
+    prev = None
+    curr = head
+    while curr:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    return prev
+
+# head = reverseList(head)
+# head.printList()
+
+
+def removeNodes(head):
+    head = reverseList(head)
+    head.printList()
+    maximum = head.val
+    ptr = head
+    while ptr.next:
+        if ptr.next.val >= maximum:
+            maximum = ptr.next.val
+            ptr = ptr.next
+        else:
+            ptr.next = ptr.next.next
+    return reverseList(head)
+
+# head = removeNodes(head)
+# head.printList()
+
+s = "coaching"
+t = "coding"
+# s = 'abcde'
+# t = 'a'
+# s = 'z'
+# t = 'abcde'
+# s = 'lbg'
+# t = 'g'
+# find how much of s is in t
+t_ptr = 0
+for i, c in enumerate(s):
+    if c == t[t_ptr] and t_ptr < len(t):
+        t_ptr += 1
+    if t_ptr == len(t):
+        break
+
+
+print(len(t) - t_ptr)
+
+def appendCharacters(s,t):
+    s_ptr, t_ptr = 0, 0
+    while t_ptr < len(t):
+        if s[s_ptr] == t[t_ptr]:
+            s_ptr += 1
+        t_ptr += 1
+    
+    if s_ptr == t_ptr:
+        return 0
+    return len(t) - s_ptr
+
+# print(appendCharacters(s,t))
